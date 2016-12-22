24982231503126768f898f994fc2970b82fd6abf
Joel Messerli <joel.messerli@bit.admin.ch>
2016-12-22 15:48:20 +0100

    Added InfoContributor

---
+++ src/main/kotlin/nu/peg/demo/info/AccountInfoContributor.kt
@@ -0,0 +1,16 @@
+package nu.peg.demo.info
+
+import nu.peg.demo.account.AccountService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.info.Info
+import org.springframework.boot.actuate.info.InfoContributor
+import org.springframework.stereotype.Component
+
+@Component
+class AccountInfoContributor @Autowired constructor(val accountService: AccountService) : InfoContributor {
+    override fun contribute(builder: Info.Builder?) {
+        val allAccounts = accountService.findAll()
+        builder?.withDetail("wealth", allAccounts.map { it.balance }.sum())
+        builder?.withDetail("accounts", allAccounts.count())
+    }
+}
---
+++ src/test/kotlin/nu/peg/demo/transfer/internal/DefaultTransferServiceTest.kt
@@ -0,0 +1,66 @@
+package nu.peg.demo.transfer.internal
+
+import com.google.common.truth.Truth.assertThat
+import nu.peg.demo.account.AccountService
+import nu.peg.demo.data.account.Account
+import nu.peg.demo.data.account.AccountRepository
+import nu.peg.demo.data.transfer.TransferDtoMapper
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import javax.transaction.Transactional
+
+@SpringBootTest
+@RunWith(SpringRunner::class)
+@Transactional
+@Ignore
+class DefaultTransferServiceTest {
+    @Autowired lateinit var accountService: AccountService
+    @Autowired lateinit var accountRepository: AccountRepository
+    @Autowired lateinit var transferService: DefaultTransferService
+    @Autowired lateinit var transferDtoMapper: TransferDtoMapper
+
+    lateinit var accounts: MutableList<Account>
+
+    @Before
+    fun setUp() {
+        accounts = mutableListOf()
+        var account1: Account = accountService.createAccount()
+        account1 = accountService.loadAccount(account1.id!!, 1000.0)!!
+        accounts.add(account1)
+        accounts.add(accountService.createAccount())
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun transferWithId() {
+        val transfer = transferService.transfer(accounts[0].id!!, accounts[1].id!!, 200.0)
+        assertThat(transfer).isNotNull()
+        assertThat(accountRepository.findOne(accounts[0].id).balance).isEqualTo(800.0)
+        assertThat(accountRepository.findOne(accounts[1].id).balance).isEqualTo(200.0)
+    }
+
+    @Test
+    fun transferWithInstance() {
+        val transfer = transferService.transfer(accounts[0], accounts[1], 200.0)
+        assertThat(transfer).isNotNull()
+        assertThat(accountRepository.findOne(accounts[0].id).balance).isEqualTo(800.0)
+        assertThat(accountRepository.findOne(accounts[1].id).balance).isEqualTo(200.0)
+    }
+
+    @Test
+    fun findAll() {
+        val findAll = transferService.findAll()
+        assertThat(findAll).isEmpty()
+    }
+
+}
