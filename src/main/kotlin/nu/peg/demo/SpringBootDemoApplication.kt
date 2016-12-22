193f03772a7e6e99f4111e61fe3c3dfa51458eab
Joel Messerli <joel.messerli@bit.admin.ch>
2016-12-22 16:35:00 +0100

    Now generating a non-standalone war

--- src/main/kotlin/nu/peg/demo/SpringBootDemoApplication.kt
+++ src/main/kotlin/nu/peg/demo/SpringBootDemoApplication.kt
@@ -2,9 +2,10 @@ package nu.peg.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.support.SpringBootServletInitializer
 
 @SpringBootApplication
-open class SpringBootDemoApplication
+open class SpringBootDemoApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     SpringApplication.run(SpringBootDemoApplication::class.java, *args)
